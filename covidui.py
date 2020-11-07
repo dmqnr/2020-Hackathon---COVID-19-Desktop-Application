5755badd596dc5b145b9f46d2591343378f56607
Daniel Marin <danirlmarin875@gmail.com>
2020-11-07 17:32:23 -0600

    Update covidui.py

--- covidui.py
+++ covidui.py
@@ -24,7 +24,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("Covid-19 Travel Advisory")
         MainWindow.resize(800, 600)
-        self.setWindowTitle("COVID-19 Travel Advisory") 
+        MainWindow.setWindowTitle("COVID-19 Travel Advisory") 
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.countryList = QtWidgets.QComboBox(self.centralwidget)
