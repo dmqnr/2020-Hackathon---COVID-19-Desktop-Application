f721815530f2da6ca523ba6aff54113aa2af6738
Daniel Marin <danirlmarin875@gmail.com>
2020-11-08 00:42:03 -0600

    Update searchByCountryClass.py
    Added checkNone function

--- searchByCountryClass.py
+++ searchByCountryClass.py
@@ -2,6 +2,9 @@ import json
 import requests
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+def checkNone(data):
+    if data is None:
+        data = "0"
 
 def searchByCountry(country):
     querystring = {"country" : country}
@@ -12,14 +15,34 @@ def searchByCountry(country):
     }
     response = requests.request("GET", url, headers=headers, params=querystring)
     data = response.json()
+    
     popul = data["response"][0]["population"]
+    checkNone(popul)
+    
     active = data["response"][0]["cases"]["active"]
+    checkNone(active)
+    
     newCases = data["response"][0]["cases"]["new"]
+    checkNone(newCases)
+    
     critical = data["response"][0]["cases"]["critical"]
+    checkNone(critical)
+    
     recovered = data["response"][0]["cases"]["recovered"] 
+    checkNone(recovered)
+    
     totalCases = data["response"][0]["cases"]["total"] 
+    checkNone(totalCases)
+    
     newDeaths = data["response"][0]["deaths"]["new"]
+    checkNone(newDeaths)
+    
     totalDeaths = data["response"][0]["deaths"]["total"]
+    checkNone(totalDeaths)
+    
     totalTests = data["response"][0]["tests"]["total"]
+    checkNone(totalTests)
+    
     date = data["response"][0]["day"]
+
     return popul, active, newCases, critical, recovered, totalCases, newDeaths, totalDeaths, totalTests, date
